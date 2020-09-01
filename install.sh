ec42e8b1024f08d350ce3beff77e3bc7f403c329
Logan Hennessy <loganjhennessy@gmail.com>
2020-08-31 20:03:41 -0700

    Add python install.

--- install.sh
+++ install.sh
@@ -2,7 +2,27 @@
 #
 #
 ## Homebrew installation
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+which -s brew
+if [[ $? != 0 ]] ; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+    brew update
+fi
+
+ver=$(python -c "import sys; print(sys.version_info.major)")
+if [ $ver -eq 2 ]; then
+    brew install python
+elif [ $ver -eq 3 ]; then
+    echo "Python3 already installed"
+else 
+    echo "Unknown python version: $ver"
+fi
+
+# DOWNSTREAM INSTALLATIONS
+#
+#
+## Brew bundle, installs all homebrew binaries, depends on `Brewfile` copied in previous step
+brew bundle install
 
 ## oh-my-zsh Installation
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -13,8 +33,3 @@
 ## Copy dotfiles and configs to home directory
 python bootstrap.py
 
-# DOWNSTREAM INSTALLATIONS
-#
-#
-## Brew bundle, installs all homebrew binaries, depends on `Brewfile` copied in previous step
-brew bundle install
